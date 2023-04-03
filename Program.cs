b205a007779cb71a466e3c6ab60dc476aaf49f80
uttamdasindusnet <118423109+uttamdasindusnet@users.noreply.github.com>
2023-04-03 20:51:28 +0530

    fdgfgf

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
- // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. uttam das
+ // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. uttam das komal
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
