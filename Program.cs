21854fecb110e9f9a9a5a3e976776519d59ace81
komalkumariint <komal.kumari@indusnet.co.in>
2023-04-04 16:08:23 +0530

    changed Userendpoint  by komal 16:08

--- Program.cs
+++ Program.cs
@@ -16,8 +16,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
-
+app.UseRouting()
+    .UseEndpoints(endpoints =>)
+    .UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllerRoute(
